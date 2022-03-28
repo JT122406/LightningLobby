6c67b7ebf0ee1729f967fb17c8085a441d4bc572
J.T. McQuigg <J.T.McQuigg12@gmail.com>
2022-03-28 18:52:48 -0400

    Null check

--- src/main/kotlin/lightninglobby.lightninglobby/cpgames.kt
+++ src/main/kotlin/lightninglobby.lightninglobby/cpgames.kt
@@ -10,6 +10,8 @@ import org.bukkit.event.inventory.InventoryClickEvent
 class cpgames : Listener {
     @EventHandler
     fun clickgame(event: InventoryClickEvent) {
+        if (event.currentItem == null)
+            return;
         val p = event.whoClicked as Player
         if (p.location.world.name == "Minigames_Lobby" && event.currentItem!!.type == Material.COMPASS) {
             Bukkit.getServer().dispatchCommand(p, "cp Games")
